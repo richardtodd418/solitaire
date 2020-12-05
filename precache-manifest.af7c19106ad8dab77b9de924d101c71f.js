dee49a898039828e3b4f2f1d9b00f972c0526e45
Richard Todd <17915329+richardtodd418@users.noreply.github.com>
2020-12-05 16:39:04 -0500

    Updates

---
+++ precache-manifest.af7c19106ad8dab77b9de924d101c71f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695b5b877b5a94f27e54f1a6ba7e3ef5",
+    "url": "/richardtodd418/solitaire/index.html"
+  },
+  {
+    "revision": "4f2d2586b5c028709ce2",
+    "url": "/richardtodd418/solitaire/static/css/2.a6f3bfa2.chunk.css"
+  },
+  {
+    "revision": "4f2d2586b5c028709ce2",
+    "url": "/richardtodd418/solitaire/static/js/2.6653bd58.chunk.js"
+  },
+  {
+    "revision": "7f51c46574b2570c404942959aca69d0",
+    "url": "/richardtodd418/solitaire/static/js/2.6653bd58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e72d5c6373be4054b8a",
+    "url": "/richardtodd418/solitaire/static/js/main.736c31ae.chunk.js"
+  },
+  {
+    "revision": "1ae3f4afbe06a2d7939a",
+    "url": "/richardtodd418/solitaire/static/js/runtime-main.69b14adc.js"
+  }
+]);
